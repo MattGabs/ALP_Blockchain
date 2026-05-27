262088bdb3633d12b3761496a7b099a12f8491a4
MattGabs <153157349+MattGabs@users.noreply.github.com>
2026-05-27 11:02:40 +0700

    Initial commit

---
+++ smart-contract/hardhat.config.ts
@@ -0,0 +1,9 @@
+import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem"; 
+import { defineConfig } from "hardhat/config"; 
+
+export default defineConfig({ 
+  plugins: [hardhatToolboxViemPlugin], 
+  solidity: { 
+    version: "0.8.28", 
+  }, 
+});
