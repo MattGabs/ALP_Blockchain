652453cd1d4edfc4e1d21f99729e8f9a6c64a387
nyanakumaro123 <nkisunyoto123@gmail.com>
2026-06-03 10:07:08 +0700

    update

--- smart-contract/hardhat.config.ts
+++
@@ -1,9 +0,0 @@
-import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem"; 
-import { defineConfig } from "hardhat/config"; 
-
-export default defineConfig({ 
-  plugins: [hardhatToolboxViemPlugin], 
-  solidity: { 
-    version: "0.8.28", 
-  }, 
-});
